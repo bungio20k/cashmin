32d56fdf193bfb0c5483707903cc44b97af7f7cb
nnhutan <nhutan2001@gmail.com>
2022-04-17 11:46:39 +0700

    Add category screen

---
+++ src/styles/category/ModalStyle.js
@@ -0,0 +1,38 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 28,
+    fontWeight: "400",
+    color: "#3C896D",
+    marginBottom: 16,
+  },
+  iconSelection: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  selection: {
+    flexDirection: "row",
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    alignItems: "center",
+  },
+  iconText: {
+    marginRight: 24,
+  },
+
+  actions: {
+    flexDirection: "row",
+    marginTop: 24,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  confirmText: {
+    fontSize: 18,
+    fontWeight: "700",
+    textAlign: "center",
+    marginVertical: 16,
+  },
+});
+
+export default styles;
