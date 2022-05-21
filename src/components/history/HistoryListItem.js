3715d65b05c05b5dffc533141c90cb7cc8174913
nnhutan <nhutan2001@gmail.com>
2022-05-21 12:12:12 +0700

    update report and history screen

--- src/components/history/HistoryListItem.js
+++ src/components/history/HistoryListItem.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, Image, StyleSheet } from "react-native";
 
-import Theme from 'src/theme/mainTheme';
-import Typo from 'src/theme/mainTypo';
+import Theme from "src/theme/mainTheme";
+import Typo from "src/theme/mainTypo";
+import { Ionicons } from "@expo/vector-icons";
 
 export function HistoryListItem({ data }) {
   return (
     <View style={st.container}>
-      <Image
-        style={st.icon}
-        source={require("../../../assets/favicon.png")} />
+      <Ionicons name={data.icon} size={30} color="#198155" />
       <View style={st.textContainer}>
-        <Text style={[Typo.body, st.textDescription]}>{data.description}</Text>
+        <Text style={st.description}>{data.name}</Text>
         <Text style={st.textTime}>1/1/2022</Text>
       </View>
       <View style={st.amountContainer}>
-        <Text>{data.amount}</Text>
+        <Text
+          style={{
+            color: (data.amount.includes("-") && "red") || "green",
+            fontWeight: "700",
+          }}
+        >
+          {data.amount}
+        </Text>
       </View>
     </View>
   );
@@ -23,29 +29,47 @@ export function HistoryListItem({ data }) {
 
 const st = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    marginHorizontal: '4%',
-    marginBottom: 7,
+    flexDirection: "row",
+    marginHorizontal: "5%",
+    marginBottom: 8,
+    paddingVertical: 12,
+    paddingHorizontal: 8,
+    borderRadius: 10,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+
+    elevation: 2,
     backgroundColor: Theme.lightGreen,
-    width: '92%'
+    width: "90%",
   },
   icon: {
     flex: 1,
-    margin: 10
+    margin: 10,
   },
   textContainer: {
     flex: 3,
-    flexDirection: 'column',
-    justifyContent: 'space-around'
+    flexDirection: "column",
+    justifyContent: "space-around",
+    marginLeft: 8,
   },
-    textDescription: {
-    },
-    textTime: {
-
-    },
+  description: {
+    fontSize: 16,
+  },
+  textTime: {
+    fontSize: 12,
+    color: "#555",
+    fontStyle: "italic",
+  },
   amountContainer: {
     flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
+    justifyContent: "center",
+    alignItems: "center",
+    fontSize: 16,
+    fontWeight: "700",
+  },
 });
--- src/screens/AppScreens/MoreTab/ReportHistoryScreen.js
+++ src/screens/AppScreens/MoreTab/ReportHistoryScreen.js
@@ -1,53 +1,47 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, ScrollView, StatusBar, FlatList, Button } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  StatusBar,
+  FlatList,
+  Button,
+  SafeAreaView,
+} from "react-native";
 
-import { Select } from 'native-base';
-import { Picker } from '@react-native-picker/picker';
-import { VictoryChart, VictoryGroup, VictoryBar, VictoryTheme } from 'victory-native';
+import { HStack, Select, VStack } from "native-base";
+import { Picker } from "@react-native-picker/picker";
+import {
+  VictoryChart,
+  VictoryGroup,
+  VictoryBar,
+  VictoryTheme,
+} from "victory-native";
 
-import Theme from 'src/theme/mainTheme';
-import Typo from 'src/theme/mainTypo';
-import { HistoryListItem } from 'src/components/history/HistoryListItem';
-
-
+import Theme from "src/theme/mainTheme";
+import Typo from "src/theme/mainTypo";
+import { HistoryListItem } from "src/components/history/HistoryListItem";
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 // Data (TODO: get from database)
 
 const DATA = [
-  {
-    id: '1',
-    description: "aaaaaaaaa",
-    amount: "+100,000 đ"
-  },
-  {
-    id: '2',
-    description: "bbbbbbbbb",
-    amount: "-150,000 đ"
-  },
-  {
-    id: '3',
-    description: "ccccccccc",
-    amount: "+500,000 đ"
-  },
-  {
-    id: '4',
-    description: "ddddddddd",
-    amount: "-1,000,000 đ"
-  },
-  {
-    id: '5',
-    description: "eeeeeeeee",
-    amount: "+1,000,000 đ"
-  },
-  {
-    id: '6',
-    description: "fffffffff",
-    amount: "-100,000 đ"
-  },
-  {
-    id: '7',
-    description: "ggggggggg",
-    amount: "+150,000 đ"
-  }
+  { icon: "fast-food", name: "Ăn sáng", amount: "-10.000 VNĐ" },
+  { icon: "fast-food", name: "Ăn trưa", amount: "-25.000 VNĐ" },
+  { icon: "fast-food", name: "Ăn tối", amount: "-15.000 VNĐ" },
+  { icon: "cafe", name: "Cà phê", amount: "-25.000 VNĐ" },
+  { icon: "car", name: "Chạy xe ôm", amount: "+90.000 VNĐ" },
+  { icon: "basket", name: "Bán hàng", amount: "+125.000 VNĐ" },
+  { icon: "shirt", name: "Bán áo, quần", amount: "+225.000 VNĐ" },
+  { icon: "cafe", name: "Cà phê", amount: "-25.000 VNĐ" },
+  { icon: "car", name: "Xăng", amount: "-80.000 VNĐ" },
+  { icon: "car", name: "Sửa xe", amount: "-60.000 VNĐ" },
+  { icon: "car", name: "Rửa xe", amount: "-40.000 VNĐ" },
+  { icon: "home", name: "Tiền thuê trọ", amount: "-30.000 VNĐ" },
+  { icon: "home", name: "Đồ dùng sinh hoạt", amount: "-110.000 VNĐ" },
+  { icon: "home", name: "Bảo trì phòng", amount: "-50.000 VNĐ" },
+  { icon: "basket", name: "Đi chợ", amount: "-200.000 VNĐ" },
+  { icon: "shirt", name: "Shopping", amount: "-100.000 VNĐ" },
 ];
 
 const toGraphData = (data) => {
@@ -58,264 +52,285 @@ const graphData = {
   in: [
     {
       id: 1,
-      amount: 10
+      amount: 10,
     },
     {
       id: 3,
-      amount: 50
+      amount: 50,
     },
     {
       id: 5,
-      amount: 100
+      amount: 100,
     },
     {
       id: 7,
-      amount: 15
-    }
+      amount: 15,
+    },
   ],
   out: [
     {
       id: 2,
-      amount: 15
+      amount: 15,
     },
     {
       id: 4,
-      amount: 100
+      amount: 100,
     },
     {
       id: 6,
-      amount: 10
-    }
-  ]
+      amount: 10,
+    },
+  ],
 };
 
-var graphViewY; var graphViewHeight;
+var graphViewY;
+var graphViewHeight;
 
 // Screen
 export default function ReportHistoryScreen() {
+  const tabBarHeight = useBottomTabBarHeight();
   const [selectedTime, setSelectedTime] = useState("week");
   const [selectedWallet, setSelectedWallet] = useState("Ví #1");
   return (
-    <View style={st.container}>
-      <View style={st.reportContainer}>
-        <Text style={[Typo.h4, st.reportHeader]}>Thống kê</Text>
+    <View style={[st.container, { marginBottom: tabBarHeight }]}>
+      <ScrollView nestedScrollEnabled>
+        <View style={st.reportContainer}>
+          <Text style={[Typo.h4, st.reportHeader]}>Thống kê</Text>
+          <View style={st.btnContainer}>
+            <Select
+              w="90"
+              py="0"
+              px="2"
+              marginX="1"
+              zIndex={3}
+              borderColor="transparent"
+              backgroundColor="#4FB286"
+              color="white"
+              fontSize="16"
+              borderRadius="full"
+              selectedValue={selectedTime}
+              onValueChange={(itemValue) => {
+                setSelectedTime(itemValue);
+              }}
+              _selectedItem={{
+                bg: "teal.600",
+              }}
+            >
+              <Select.Item label="Tuần" value="week" />
+              <Select.Item label="Tháng" value="month" />
+              <Select.Item label="Năm" value="year" />
+            </Select>
+            <Select
+              w="90"
+              py="0"
+              px="2"
+              marginX="1"
+              zIndex={3}
+              borderColor="transparent"
+              backgroundColor="#4FB286"
+              color="white"
+              fontSize="16"
+              borderRadius="full"
+              selectedValue={selectedWallet}
+              onValueChange={(itemValue) => {
+                setSelectedWallet(itemValue);
+              }}
+              _selectedItem={{
+                bg: "teal.600",
+              }}
+            >
+              <Select.Item label="Ví #1" value="Ví #1" />
+              <Select.Item label="Ví #2" value="Ví #2" />
+              <Select.Item label="Ví #3" value="Ví #3" />
+            </Select>
+          </View>
 
-        <View style={st.btnContainer}>
-          {/* <Picker selectedValue={selectedTime}
-                  onValueChange={(itemValue, itemIndex) => setSelectedTime(itemValue)}
-                  style={st.picker}>
-            <Picker.Item label="Tuần" value="week" />
-            <Picker.Item label="Tháng" value="month" />
-            <Picker.Item label="Năm" value="year" />
-          </Picker> */}
-          <Select
-            w="86px"
-            h="66%"
-            p="0"
-            marginX="1"
-            borderColor="transparent"
-            borderBottomColor="#888"
-            borderRadius="none"
-            marginHorizontal="10%"
-            selectedValue={selectedTime}
-            onValueChange={(itemValue) => {
-              setSelectedTime(itemValue);
-            }}
-            _selectedItem={{
-              bg: "teal.600",
+          <View
+            style={st.graph}
+            onLayout={(event) => {
+              // var layout = event.nativeEvent.layout;
+              // graphViewY = layout.y;
+              // graphViewHeight = layout.height;
+              // console.log("Y = " + graphViewY + " + " + graphViewHeight);
             }}
           >
-            <Select.Item label="Tuần" value="week" />
-            <Select.Item label="Tháng" value="month" />
-            <Select.Item label="Năm" value="year" />
-          </Select>
+            <VictoryChart
+              width={360}
+              // height={220}
+              //height={graphViewY + graphViewHeight}
 
-          <Select
-            w="86px"
-            h="66%"
-            p="0"
-            marginX="1"
-            borderColor="transparent"
-            borderBottomColor="#888"
-            borderRadius="none"
-            selectedValue={selectedWallet}
-            onValueChange={(itemValue) => {
-              setSelectedWallet(itemValue);
-            }}
-            _selectedItem={{
-              bg: "teal.600",
-            }}
-          >
-            <Select.Item label="Ví #1" value="Ví #1" />
-            <Select.Item label="Ví #2" value="Ví #2" />
-            <Select.Item label="Ví #3" value="Ví #3" />
-          </Select>
-
-          {/* <Picker selectedValue={selectedWallet}
-                  onValueChange={(itemValue, itemIndex) => setSelectedWallet(itemValue)}
-                  style={st.picker}>
-            <Picker.Item label="Ví #1" value="Ví #1" />
-            <Picker.Item label="Ví #2" value="Ví #2" />
-            <Picker.Item label="Ví #3" value="Ví #3" />
-          </Picker> */}
-        </View>
-
-        <View style={st.graph}
-          onLayout={(event) => {
-            // var layout = event.nativeEvent.layout;
-            // graphViewY = layout.y;
-            // graphViewHeight = layout.height; 
-            // console.log("Y = " + graphViewY + " + " + graphViewHeight); 
-          }}>
-          <VictoryChart
-            width={360}
-            height={260}
-            //height={graphViewY + graphViewHeight}
-            
-            domain={{ x: [0, graphData.in.length + graphData.out.length], y: [0, 275] }}
-            //theme={VictoryTheme.material}
-            style={{
-              chart: {
-                //width: '30%'
-                // flex: 0.5,
-                // height: '50%'
-              }
-            }}>
-              <VictoryGroup
-                offset={15}>
-                <VictoryBar data={graphData.in} x="id" y="amount" 
-                            style={{ data: { fill: "#009900" }}}/>
-                <VictoryBar data={graphData.out} x="id" y="amount" 
-                            style={{ data: { fill: "#bf0000" }}}/>
+              domain={{
+                x: [0, graphData.in.length + graphData.out.length],
+                y: [0, 275],
+              }}
+              //theme={VictoryTheme.material}
+              style={{
+                chart: {
+                  //width: '30%'
+                  // flex: 0.5,
+                  // height: '50%'
+                },
+              }}
+            >
+              <VictoryGroup offset={15}>
+                <VictoryBar
+                  data={graphData.in}
+                  x="id"
+                  y="amount"
+                  style={{ data: { fill: "#009900" } }}
+                />
+                <VictoryBar
+                  data={graphData.out}
+                  x="id"
+                  y="amount"
+                  style={{ data: { fill: "#bf0000" } }}
+                />
               </VictoryGroup>
             </VictoryChart>
-        </View>
-        
-        <View style={st.summaryContainer}>
-          <View style={st.summaryRowContainer}>
-            <View style={st.summaryRow}>
-              <View>
-                <Text style={st.textCenter}>Tổng thu:</Text>
-                <Text style={st.textCenter}>100,000 đ</Text>
-              </View>
-            </View>
-            <View style={st.summaryRow}>
-              <View>
-                <Text style={st.textCenter}>Tổng chi:</Text>
-                <Text style={st.textCenter}>-200,000 đ</Text>
-              </View>
-            </View>
           </View>
-          <View style={st.summaryRowContainer}>
-            <View style={st.summaryRow}>
-              <View>
-                <Text style={st.textCenter}>Tổng thu chi:</Text>
-                <Text style={st.textCenter}>-100,000 đ</Text>
+
+          <VStack space={1} alignItems="center">
+            <HStack space={6} justifyContent="center">
+              <View style={st.incomeMoneyContainer}>
+                <Text style={st.moneyTitle}>Tổng thu</Text>
+                <Text style={st.incomeMoney}>1.600.000 VNĐ</Text>
               </View>
+              <View
+                style={{
+                  justifyContent: "center",
+                }}
+              >
+                <Text style={{ fontWeight: "700" }}>---</Text>
+              </View>
+              <View style={st.expenseMoneyContainer}>
+                <Text style={st.moneyTitle}>Tổng chi</Text>
+                <Text style={st.expenseMoney}>800.000 VNĐ</Text>
+              </View>
+            </HStack>
+            <View style={st.totalContainer}>
+              <Text style={st.moneyTitle}> Tổng thu - chi</Text>
+              <Text style={st.incomeMoney}>800.000 VNĐ</Text>
             </View>
-          </View>
+          </VStack>
         </View>
-      </View>
-      
-      <View style={st.historyContainer}>
-        <Text style={[Typo.h4, st.historyHeader]}>Lịch sử</Text>
-        <View style={st.historyListContainer}>
-          <FlatList
+        <View
+          style={{
+            height: 8,
+            backgroundColor: "white",
+            width: "100%",
+            borderRadius: 8,
+            marginTop: 8,
+          }}
+        ></View>
+        <View style={[st.historyContainer, { marginBottom: tabBarHeight }]}>
+          <Text style={[Typo.h4, st.historyHeader]}>Lịch sử</Text>
+          <View style={st.historyListContainer}>
+            <SafeAreaView>
+              <ScrollView nestedScrollEnabled>
+                {DATA.map((item, index) => (
+                  <HistoryListItem data={item} key={index} />
+                ))}
+              </ScrollView>
+            </SafeAreaView>
+
+            {/* <FlatList
             data={DATA}
             keyExtractor={(item) => item.id}
-            renderItem={({item}) => {
-              return <HistoryListItem data={item} />
-            }} 
-            contentContainerStyle={st.historyList}>
-
-          </FlatList>
+            renderItem={({ item }) => {
+              return <HistoryListItem data={item} />;
+            }}
+            contentContainerStyle={st.historyList}
+          ></FlatList> */}
+          </View>
+          <View style={st.historyEmpty}></View>
         </View>
-        <View style={st.historyEmpty}></View>
-      </View>
-
+      </ScrollView>
     </View>
   );
 }
 
 const st = StyleSheet.create({
-  textCenter: {
-    textAlign: 'center',
-    textAlignVertical: 'center'
-  },
-
   container: {
     paddingTop: StatusBar.currentHeight,
     flex: 1,
     backgroundColor: Theme.background,
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    alignItems: "center",
+    justifyContent: "space-between",
   },
 
   reportContainer: {
-    flex: 0.55,
-    width: '100%'
+    // flex: 0.55,
+    // height: "50%",
+    width: "100%",
   },
-    reportHeader: {
-      flex: 0.7,
-      textAlign: 'center',
-      textAlignVertical: 'center',
-      color: Theme.black,
-      backgroundColor: Theme.mint
-    },
+  reportHeader: {
+    // flex: 0.7,
+    textAlign: "center",
+    textAlignVertical: "center",
+    fontSize: 24,
+    fontWeight: "700",
+    color: "#198155",
+    marginVertical: 4,
+    // backgroundColor: Theme.mint,
+  },
 
-    btnContainer: {
-      flex: 0.7,
-      width: '100%',
-      flexDirection: 'row',
-      justifyContent: 'center'
-    },
-      picker: {
-        width: '31%',
-        marginHorizontal: '2%',
-        backgroundColor: '#008000',
-        color: '#800000'
-      },
+  btnContainer: {
+    // flex: 0.7,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
 
-    graph: {
-      flex: 4.5,
-      justifyContent: 'center',
-      alignItems: 'center',
-      width: '100%'
-    },
-
-    summaryContainer: {
-      flex: 2,
-      justifyContent: 'center'
-    },
-      summaryRowContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-evenly'
-      },
-        summaryRow: {
-          flexDirection: 'column',
-          width: '25%'
-        },
-
+  graph: {
+    // flex: 4.5,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    marginTop: -40,
+    marginBottom: -20,
+  },
 
   historyContainer: {
-    flex: 0.45,
-    width: '100%',
+    // flex: 0.45,
+    height: 580,
+    width: "100%",
   },
-    historyHeader: {
-      flex: 1,
-      textAlign: 'center',
-      textAlignVertical: 'center',
-      color: Theme.black,
-      backgroundColor: Theme.mint
-    },
-    historyListContainer: {
-      flex: 8.5
-    },
-    historyList: {
-      justifyContent: 'center',
-      alignItems: 'flex-start'
-    },
-    historyEmpty: {
-      flex: 1.5
-    }
+  historyHeader: {
+    // flex: 1,
+    textAlign: "center",
+    textAlignVertical: "center",
+    color: Theme.black,
+    fontSize: 24,
+    fontWeight: "700",
+    color: "#198155",
+    marginBottom: 8,
+    marginTop: 12,
+    // backgroundColor: Theme.mint
+  },
+  historyListContainer: {
+    // flex: 8.5,
+    // paddingHorizontal: 12,
+  },
+
+  historyEmpty: {
+    // flex: 1.5,
+  },
+
+  moneyTitle: {
+    fontSize: 16,
+  },
+  incomeMoney: {
+    fontSize: 16,
+    color: "green",
+    fontWeight: "700",
+  },
+
+  expenseMoney: {
+    fontSize: 16,
+    color: "red",
+    fontWeight: "700",
+  },
+  moneyTitle: {
+    color: "#222",
+  },
 });
