789638101e846148f42ff3426ef89508982c8fce
nnhutan <nhutan2001@gmail.com>
2022-05-25 09:56:37 +0700

    tan commit

--- App.js
+++ App.js
@@ -11,8 +11,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { DataProvider } from "./src/hooks/data/DataContext";
 
 // Axios config
-import axios from 'axios';
-axios.defaults.baseURL = 'http://192.168.137.1:3001/api/v1';
+import axios from "axios";
+// axios.defaults.baseURL = 'http://192.168.137.1:3001/api/v1';
+axios.defaults.baseURL = "http://192.168.1.4:3001/api/v1";
 
 export default function App() {
   return (
--- src/components/home/HomeTop.js
+++ src/components/home/HomeTop.js
@@ -3,13 +3,13 @@ import { Ionicons } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
 import React, { useContext, useState } from "react";
 import styles from "../../styles/home/HomeTopStyle";
-import DataContext from '../../hooks/data/DataContext'
+import DataContext from "../../hooks/data/DataContext";
 
 const HomeTop = () => {
   const [hideMoney, setHideMoney] = useState(false);
   const { profile, wallets, settings } = useContext(DataContext);
-  const mainWalletBalance = wallets.find((w) => w.isMain).balance;
-  
+  const mainWalletBalance = wallets?.find((w) => w.isMain)?.balance || 0;
+
   return (
     <View style={styles.top}>
       <View style={styles.title}>
@@ -22,7 +22,7 @@ const HomeTop = () => {
         <View style={styles.content}>
           <Text style={styles.contentTitle}>Tổng số dư</Text>
           <Text style={styles.money}>
-            {hideMoney ? "***.***.***" : mainWalletBalance} {settings.currency}
+            {hideMoney ? "*********" : mainWalletBalance} {settings.currency}
           </Text>
         </View>
         <TouchableOpacity
