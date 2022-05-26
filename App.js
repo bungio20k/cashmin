e1072d80642de074e4beaec8677098ea8891f8b4
nnhutan <nhutan2001@gmail.com>
2022-05-26 22:27:12 +0700

    tan commit

--- App.js
+++ App.js
@@ -7,22 +7,17 @@ import AuthRedirect from "./src/containers/AuthRedirect";
 // Navigation
 import { NavigationContainer } from "@react-navigation/native";
 
-// data
-import { DataProvider } from "./src/hooks/data/DataContext";
-
 // Axios config
 import axios from "axios";
 // axios.defaults.baseURL = 'http://192.168.137.1:3001/api/v1';
-axios.defaults.baseURL = "http://192.168.1.4:3001/api/v1";
+axios.defaults.baseURL = "http://192.168.1.2:3001/api/v1";
 
 export default function App() {
   return (
     <AuthProvider>
       <NativeBaseProvider>
         <NavigationContainer>
-          <DataProvider>
-            <AuthRedirect />
-          </DataProvider>
+          <AuthRedirect />
         </NavigationContainer>
       </NativeBaseProvider>
     </AuthProvider>
